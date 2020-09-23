a80d77d25585b83f915cc33ce47d5e8f0fb6d2d0
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-09-22 22:18:46 -0300

    Created home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+
+import Opening from './Home/Opening'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Code editing by <code>Rodrigo da Silva</code>
-        </p>
-      </header>
+    <div className='h-100'>
+      <Opening/>
     </div>
   );
 }
---
+++ src/Home/Opening.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Grid, Header, Segment, Card, Button, GridRow, GridColumn } from 'semantic-ui-react'
+
+const Opening = props => {
+    return (
+        <Grid container columns='2' verticalAlign='middle' textAlign='center' className='h-75'>
+            <GridRow>
+                <GridColumn>
+                    <Segment piled>
+                        Quiz
+                    </Segment>
+                    <Header as='h2'>
+                        A game of questions and answers.
+                        </Header>
+                    <p>
+                        Challenge your friends on this awesome quiz-game. Just share with them and let the fun go.
+                        </p>
+                    <Card fluid>
+                        <Card.Content>
+                            Join now
+                            </Card.Content>
+                        <Card.Content>
+                            <Button color='blue'>Login with Facebook</Button>
+                            <Button color='green'>Administrative Area</Button>
+                        </Card.Content>
+                    </Card>
+                </GridColumn>
+            </GridRow>
+        </Grid>
+    )
+}
+
+export default Opening
