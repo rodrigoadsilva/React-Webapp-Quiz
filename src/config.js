236d6bdf4ee22bd66d755e97bd906bb9adc5ea18
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-09-29 21:56:42 -0300

    Create the Firebase configuration

---
+++ src/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase'
+import Rebase from 're-base'
+
+const firebaseInfo = {
+    apiKey: "AIzaSyDguEpC4a5cW891AzqQAdXBftKqelOC1Vc",
+    authDomain: "quiz-game-udemy.firebaseapp.com",
+    databaseURL: "https://quiz-game-udemy.firebaseio.com",
+    projectId: "quiz-game-udemy",
+    storageBucket: "quiz-game-udemy.appspot.com",
+    messagingSenderId: "712036837624",
+    appId: "1:712036837624:web:8c15028225abba4436b30d"
+};
+
+const db = firebase.database(firebaseInfo)
+const config = Rebase.createClass(db)
+
+export const providers = {
+    'facebook': new firebase.auth.FacebookAuthProvider,
+}
+
+export const auth = firebaseInfo.auth()
+export default config
