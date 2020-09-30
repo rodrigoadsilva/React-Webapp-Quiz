87ef6433d0acc9a2e8d898465569fa272dc442cc
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-09-29 22:50:11 -0300

    Created Facebook Authentication

--- src/Home/Launch.js
+++ src/Home/Launch.js
@@ -1,39 +1,65 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Grid, Header, Segment, Card, Button, GridRow, GridColumn } from 'semantic-ui-react'
 
+import config, {auth, providers} from './../config'
 import Navbar from './Navbar'
 
-const Launch = props => {
-    return (
-        <>
-        <Navbar/>
-        <Grid container columns='2' verticalAlign='middle' textAlign='center' className='h-75'>
-            <GridRow>
-                <GridColumn>
-                    <Segment piled>
-                        Quiz
-                    </Segment>
-                    <Header as='h2'>
-                        A game of questions and answers.
-                    </Header>
-                    <p>
-                        Challenge your friends on this awesome game. Just share with them and let the fun go.
-                    </p>
-                    <br/>
-                    <Card fluid>
-                        <Card.Content>
-                            Join now
-                        </Card.Content>
-                        <Card.Content>
-                            <Button color='facebook'>Login with Facebook</Button>
-                            <Button color='twitter'>Login with Twitter</Button>
-                        </Card.Content>
-                    </Card>
-                </GridColumn>
-            </GridRow>
-        </Grid>
-        </>
-    )
+class Launch extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            user: {},
+            isLogged: false
+        }
+
+        auth.onAuthStateChanged((user) => {
+            if(user){
+                console.log(user)
+            }
+            else{
+                console.log('Can`t login')
+            }
+        })
+    }
+
+    authenticate(provider){
+        console.log(provider)
+        auth.signInWithPopup(providers[provider])
+    }
+
+    render(){
+        return (
+            <>
+            <Navbar/>
+            <Grid container columns='2' verticalAlign='middle' textAlign='center' className='h-75'>
+                <GridRow>
+                    <GridColumn>
+                        <Segment piled>
+                            Quiz
+                        </Segment>
+                        <Header as='h2'>
+                            A game of questions and answers.
+                        </Header>
+                        <p>
+                            Challenge your friends on this awesome game. Just share with them and let the fun go.
+                        </p>
+                        <br/>
+                        <Card fluid>
+                            <Card.Content>
+                                Join now
+                            </Card.Content>
+                            <Card.Content>
+                                <Button color='facebook' onClick={()=> this.authenticate('facebook')}>Login with Facebook</Button>
+                                <Button color='twitter'>Login with Twitter</Button>
+                            </Card.Content>
+                        </Card>
+                    </GridColumn>
+                </GridRow>
+            </Grid>
+            </>
+        )
+    }
 }
 
 export default Launch
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
 import Rebase from 're-base'
 
-const firebaseInfo = {
+const firebaseInfo = firebase.initializeApp({
     apiKey: "AIzaSyDguEpC4a5cW891AzqQAdXBftKqelOC1Vc",
     authDomain: "quiz-game-udemy.firebaseapp.com",
     databaseURL: "https://quiz-game-udemy.firebaseio.com",
@@ -9,13 +9,13 @@ const firebaseInfo = {
     storageBucket: "quiz-game-udemy.appspot.com",
     messagingSenderId: "712036837624",
     appId: "1:712036837624:web:8c15028225abba4436b30d"
-};
+})
 
 const db = firebase.database(firebaseInfo)
 const config = Rebase.createClass(db)
 
 export const providers = {
-    'facebook': new firebase.auth.FacebookAuthProvider,
+    'facebook': new firebase.auth.FacebookAuthProvider(),
 }
 
 export const auth = firebaseInfo.auth()
